184c3f70d7ac581077dba9df08bbd6506045d62f
nazariikravets <goodtovar1@gmail.com>
2022-02-03 17:52:00 +0200

    add classwork1

---
+++ lesson1/main2.js
@@ -0,0 +1,49 @@
+let a = [22, 33, 44, 65, 23, 45, 67, 12, 89, 1]
+console.log(a)
+let result = a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9]
+console.log(result)
+
+
+let book = {
+    name: `Jane Eyre`,
+    numberOFpages: 653,
+    author: 'Charlotte Bronte'
+}
+let book1 = {
+    name: `Will`,
+    numberOFpages: 448,
+    author: 'Will Smith Mark Manson',
+    genre: `biography`
+}
+let book2 = {
+    name: `Will`,
+    numberOFpages: 448,
+    author: 'Will Smith Mark Manson',
+    genre: `biography`
+}
+let books = [book, book1, book2]
+console.log(books[0])
+console.log(books[1])
+console.log(books[2])
+
+
+let height = 23
+let width = 10
+let s = height * width
+console.log(s)
+
+
+
+
+let heightc = 10
+let dc = 4
+let rc = dc / 2
+const Pi = 3.14
+let V = Pi * rc * rc * heightc
+console.log(V)
+
+
+let n=3
+let m=4
+let k=Math.pow(Math.pow(3,2)+Math.pow(4,2),0.5)
+console.log(k)
