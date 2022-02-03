ff8a3b28496a4c1ca821138943ea44db23d74f2b
nazariikravets <goodtovar1@gmail.com>
2022-02-03 15:18:43 +0200

    add homework1

---
+++ lesson1/main.js
@@ -0,0 +1,129 @@
+let a = "hello";
+let b = "owu";
+let c = "com";
+let m = "ua";
+let d = 1;
+let e = 10;
+let f = -999;
+let g = 123;
+let j = 3.14;
+let k = 2.7;
+let z = 16;
+let x = true;
+let v = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(m);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(j);
+console.log(k);
+console.log(z);
+console.log(x);
+console.log(v);
+document.write(`<div>${a}</div>`)
+document.write(`<div>${b}</div>`)
+document.write(`<div>${c}</div>`)
+document.write(`<div>${m}</div>`)
+document.write(`<div>${d}</div>`)
+document.write(`<div>${e}</div>`)
+document.write(`<div>${f}</div>`)
+document.write(`<div>${g}</div>`)
+document.write(`<div>${j}</div>`)
+document.write(`<div>${k}</div>`)
+document.write(`<div>${z}</div>`)
+document.write(`<div>${x}</div>`)
+document.write(`<div>${v}</div>`)
+alert(a)
+alert(b)
+alert(c)
+alert(m)
+alert(d)
+alert(e)
+alert(f)
+alert(g)
+alert(j)
+alert(k)
+alert(z)
+alert(x)
+alert(v)
+a = `work`
+b = `job`
+c = `okten`
+m = 23221
+d = `nazar`
+e = -234
+f = `far`
+g = 233111
+j = 3.15
+k = -3.15
+z = false
+x = true
+v = `baraba`
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(m);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(j);
+console.log(k);
+console.log(z);
+console.log(x);
+console.log(v);
+document.write(`<div>${a}</div>`)
+document.write(`<div>${b}</div>`)
+document.write(`<div>${c}</div>`)
+document.write(`<div>${m}</div>`)
+document.write(`<div>${d}</div>`)
+document.write(`<div>${e}</div>`)
+document.write(`<div>${f}</div>`)
+document.write(`<div>${g}</div>`)
+document.write(`<div>${j}</div>`)
+document.write(`<div>${k}</div>`)
+document.write(`<div>${z}</div>`)
+document.write(`<div>${x}</div>`)
+document.write(`<div>${v}</div>`)
+alert(a)
+alert(b)
+alert(c)
+alert(m)
+alert(d)
+alert(e)
+alert(f)
+alert(g)
+alert(j)
+alert(k)
+alert(z)
+alert(x)
+alert(v)
+let name = `Nazarii`;
+let lastName = `Kravets`;
+let middleName = `Yuriyovyc`;
+let persson = [name, lastName, middleName];
+name= prompt(`What is your name?`)
+middleName= prompt(`What is your middle name ?`)
+let age= prompt(`How old are you?`)
+alert("Вітаю "+ name+" " + middleName +". Тобі "+age+" років.")
+let q=100;
+let r=`100`;
+let t=true;
+console.log(typeof q)
+console.log(typeof r)
+console.log(typeof t)
+console.log(5 < 6)
+console.log(5 > 6)
+console.log(5 === 6)
+console.log(5 >= 6)
+console.log(10 <= 10)
+console.log(10 === 10)
+console.log(10 > 10)
+console.log(10 !== 10)
+console.log(10 > 10)
+console.log(123 === `123`)
+console.log(123 == `123`)
